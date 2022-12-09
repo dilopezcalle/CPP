bf82d4eeaecc756e0041a2f864337c75cd2b4f58
Diego Lopez calle <dilopez-@c3r5s4.42urduliz.com>
2022-12-09 14:57:44 +0100

    Zombies are coming

--- Module1/ex00/main.cpp
+++ Module1/ex00/main.cpp
@@ -5,8 +5,24 @@
 
 #include <iostream>
 
+#include "Zombie.hpp"
+
+void	randomChump(std::string name);
+Zombie	*newZombie(std::string name);
+
 int	main(void)
 {
-	std::cout << "Creo una variable en heap";
+	// Test in STACK with randomChump
+	std::cout << YELLOW << "\nSe crea un Zombie en el Stack:\n" << RESET << std::endl;
+	randomChump("Paco");
+
+	// Test in HEAP with newZombie
+	Zombie	*zombie;
+	std::cout << YELLOW << "\nSe crea un Zombie en el Heap:\n" << RESET << std::endl;
+	zombie = newZombie("Lupita");
+	zombie->announce();
+
+	// Without this line, Lupita will 
+	delete zombie;
 	return (0);
 }
