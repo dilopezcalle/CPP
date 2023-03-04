1fb467a00f6c4c40627436c026764414f4a801ef
Diego Lopez calle <dilopez-@c4r1s2.42urduliz.com>
2023-03-04 10:24:13 +0100

    Moddule7 ex00, ex01, ex02 - Leaks and flags checked

--- Module7/ex00/main.cpp
+++ Module7/ex00/main.cpp
@@ -7,15 +7,20 @@
 
 #include "whatever.hpp"
 
-int main( void ) {
+int main( void )
+{
+	std::cout << YELLOW << "\nTest con enteros" << RESET << std::endl;
 	int a = 2;
 	int b = 3;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << YELLOW << "\nTest con strings" << RESET << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	std::cout << "c = " << c << ", d = " << d << std::endl;
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
